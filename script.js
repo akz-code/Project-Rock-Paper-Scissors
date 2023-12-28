39f3b8ff6bcca709a52962a12e2cdff8e8c0af19
HaKaizen <vcfzavio@gmail.com>
2023-12-28 16:51:23 -0500

    Update the program logic
    Only console.log for now

--- script.js
+++ script.js
@@ -1,95 +1,75 @@
+let computerScore = 0;
+let userScore = 0;
+
+const buttonRock = document.querySelector(".buttonRock-User");
+const buttonPaper = document.querySelector(".buttonPaper-User");
+const buttonScissors = document.querySelector(".buttonScissors-User");
+
+buttonRock.addEventListener("click", () => {
+  playGame("rock", getComputerChoice());
+});
+buttonPaper.addEventListener("click", () => {
+  playGame("papel", getComputerChoice());
+});
+buttonScissors.addEventListener("click", () => {
+  playGame("scissors", getComputerChoice());
+});
+
 // PC's choice
-function getChoicePC() {
+const getComputerChoice = function () {
   let randomChoice = Math.floor(Math.random() * 3) + 1;
+
   if (randomChoice === 1) {
-    return "Rock";
+    return "rock";
   } else if (randomChoice === 2) {
-    return "Paper";
+    return "papel";
   } else if (randomChoice === 3) {
-    return "Scissors";
+    return "scissors";
   }
-}
+};
 
-// User's choice
-function getChoiceUser() {
-  let choiceUser = parseInt(
-    prompt("Choose 1 for Rock, choose 2 for Paper and choose 3 for Scissors.")
-  );
-
-  if (
-    isNaN(choiceUser) ||
-    choiceUser < 1 ||
-    choiceUser > 3 ||
-    choiceUser === null
-  ) {
-    return null;
-  }
-
-  if (choiceUser === 1) {
-    return "Rock";
-  } else if (choiceUser === 2) {
-    return "Paper";
-  } else if (choiceUser === 3) {
-    return "Scissors";
-  }
-}
-
-// Game
-
-function Game() {
-  let PC = getChoicePC();
-  let User = getChoiceUser();
-
-  if (User === null) {
-    return null;
-  }
-
-  if (PC === User) {
-    return 0;
+// Play Game
+const playGame = function (user, computer) {
+  if (user === computer) {
+    updateScore("tie");
   } else if (
-    (PC === "Rock" && User === "Scissors") ||
-    (PC === "Paper" && User === "Rock") ||
-    (PC === "Scissors" && User === "Paper")
+    (user === "rock" && computer === "scissors") ||
+    (user === "scissors" && computer === "papel") ||
+    (user === "papel" && computer === "rock")
   ) {
-    return -1;
+    updateScore("win");
   } else if (
-    (PC === "Rock" && User === "Paper") ||
-    (PC === "Paper" && User === "Scissors") ||
-    (PC === "Scissors" && User === "Rock")
+    (user === "rock" && computer === "papel") ||
+    (user === "papel" && computer === "scissors") ||
+    (user === "scissors" && computer === "rock")
   ) {
-    return 1;
+    updateScore("lost");
   }
-}
-
-// playRound function
-
-function playRound() {
-  let UserWins = 0;
-  let PCWins = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let result = Game();
-
-    if (result === null) {
-      console.log("WTF is this? Game Over");
-      break;
-    }
-    if (UserWins === 3 || PCWins === 3) {
-      break;
-    }
-
-    if (result === 0) {
-      console.log("DRAW!");
-      console.log(`PC: ${PCWins}; User: ${UserWins}`);
-    } else if (result === 1) {
-      UserWins++;
-      console.log("You won");
-      console.log(`PC: ${PCWins}; User: ${UserWins}`);
-    } else if (result === -1) {
-      PCWins++;
-      console.log("You lost");
-      console.log(`PC: ${PCWins}; User: ${UserWins}`);
-    }
+};
+// Update score
+const updateScore = function (result) {
+  if (result === "tie") {
+    console.log(`Tie. Computer: ${computerScore} User: ${userScore}`);
+  } else if (result === "win") {
+    userScore++;
+    console.log(`You won. Computer: ${computerScore} User: ${userScore}`);
+  } else if (result === "lost") {
+    computerScore++;
+    console.log(`You lost. Computer: ${computerScore} User: ${userScore}`);
   }
-}
-playRound();
+  endGame();
+};
+
+const endGame = function () {
+  if (userScore === 5) {
+    userScore = 0;
+    computerScore = 0;
+    return alert("You won!");
+  } else if (computerScore === 5) {
+    userScore = 0;
+    computerScore = 0;
+    return alert("You lost!");
+  }
+};
+
+
